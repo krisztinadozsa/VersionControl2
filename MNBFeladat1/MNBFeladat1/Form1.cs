8875687404107c7a3414c0337cd0cf307032f6ec
Krisztina Dózsa <krisztina.dozsa@stud.uni-corvinus.hu>
2022-11-22 18:51:27 +0100

    valuta lista

--- MNBFeladat1/MNBFeladat1/Form1.cs
+++ MNBFeladat1/MNBFeladat1/Form1.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,12 @@ namespace MNBFeladat1
     public partial class Form1 : Form
     {
         BindingList<RateData> _rates = new BindingList<RateData>();
+        BindingList<string> currencies = new BindingList<string>();
         public Form1()
         {
             InitializeComponent();
+            loadCurrencyxml(getCurrencies());
+            cbValuta.DataSource = currencies;
             RefreshData();
 
             
@@ -64,14 +68,29 @@ namespace MNBFeladat1
                 RateData r = new RateData();
                 r.Date = DateTime.Parse(item.GetAttribute("date"));
                 var childElement = (XmlElement)item.ChildNodes[0];
-                r.Currency = childElement.GetAttribute("curr");
-                decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
-                r.Value = decimal.Parse(childElement.InnerText);
-                if (unit != 0)
-                    r.Value = r.Value / unit;
-                _rates.Add(r);
+                if (childElement != null)
+                {
+                    r.Currency = childElement.GetAttribute("curr");
+                    decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
+                    r.Value = decimal.Parse(childElement.InnerText);
+                    if (unit != 0)
+                        r.Value = r.Value / unit;
+                    _rates.Add(r);
+                }
             }
         }
+        private void loadCurrencyxml(string xmlstring)
+        {
+            currencies.Clear();
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlstring);
+            foreach (XmlElement item in xml.DocumentElement.ChildNodes[0])
+            {
+                string s = item.InnerText;
+                currencies.Add(s);
+            }
+        }
+
 
         private string getRates()
         {
@@ -86,7 +105,15 @@ namespace MNBFeladat1
 
         }
 
-
+        private string getCurrencies() 
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            GetCurrenciesRequestBody req = new GetCurrenciesRequestBody();
+            var resp = mnbService.GetCurrencies(req);
+            string result = resp.GetCurrenciesResult;
+            File.WriteAllText("currency.xml", result);
+            return resp.GetCurrenciesResult;
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
