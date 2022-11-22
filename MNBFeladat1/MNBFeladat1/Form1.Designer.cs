8556fe967b5d9a9436c767d9b66047ddf3f95ac4
Krisztina Dózsa <krisztina.dozsa@stud.uni-corvinus.hu>
2022-11-22 17:32:33 +0100

    XML feldolgozas

--- MNBFeladat1/MNBFeladat1/Form1.Designer.cs
+++ MNBFeladat1/MNBFeladat1/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(63, 53);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(305, 285);
+            this.dataGridView1.Size = new System.Drawing.Size(384, 359);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
--- MNBFeladat1/MNBFeladat1/Form1.cs
+++ MNBFeladat1/MNBFeladat1/Form1.cs
@@ -9,20 +9,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MNBFeladat1
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> _rates;
+        BindingList<RateData> _rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             getRates();
+            loadXml(getRates());
             dataGridView1.DataSource = _rates;
         }
 
-        private void getRates()
+        private void loadXml(string xmlstring)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlstring);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                var childElement = (XmlElement)item.ChildNodes[0];
+                r.Currency = childElement.GetAttribute("curr");
+                decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
+                r.Value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    r.Value = r.Value / unit;
+                _rates.Add(r);
+            }
+        }
+
+        private string getRates()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
@@ -30,7 +50,7 @@ namespace MNBFeladat1
             req.startDate = "2020-01-01";
             req.endDate = "2020-06-30";
             var response = mnbService.GetExchangeRates(req);
-            var result = response.GetExchangeRatesResult;
+            return response.GetExchangeRatesResult;
 
         }
 
