102c033901734501473996f5bd677b86811d7d81
Krisztina Dózsa <krisztina.dozsa@stud.uni-corvinus.hu>
2022-11-22 16:58:40 +0100

    webszolgaltatas hivasa

--- MNBFeladat1/MNBFeladat1/Form1.cs
+++ MNBFeladat1/MNBFeladat1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MNBFeladat1.MNBServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,28 @@ namespace MNBFeladat1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            getRates();
+        }
+
+        private void getRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
+            req.currencyNames = "EUR";
+            req.startDate = "2020-01-01";
+            req.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(req);
+            var result = response.GetExchangeRatesResult;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
